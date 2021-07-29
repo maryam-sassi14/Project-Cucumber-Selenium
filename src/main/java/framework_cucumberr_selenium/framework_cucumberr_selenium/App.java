9d7d9826aeb2dcfa0bc2db75219477be33fb0698
maryam-sassi14 <maryamsassimdw@gmail.com>
2021-07-29 14:00:53 +0200

    first commit

---
+++ src/main/java/framework_cucumberr_selenium/framework_cucumberr_selenium/App.java
@@ -0,0 +1,13 @@
+package framework_cucumberr_selenium.framework_cucumberr_selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
